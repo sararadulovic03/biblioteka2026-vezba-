ecba6b9a59f14c00625a3b756f7991482a1bf464
SaraRadulovic003 <radulovics003@gmail.com>
2026-04-04 11:51:31 +0200

    Kreirana klasa biblioteka

---
+++ Biblioteka(vezba)/src/biblioteka/Biblioteka.java
@@ -0,0 +1,54 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterface;
+
+public class Biblioteka implements BibliotekaInterface {
+	private List<Knjiga> knjige = new ArrayList<Knjiga>();
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+
+		if (knjige.contains(knjiga))
+			throw new IllegalArgumentException("Knjiga vec postoji");
+
+		knjige.add(knjiga);
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+
+		if (!knjige.contains(knjiga))
+			throw new IllegalArgumentException("Knjiga ne postoji");
+
+		knjige.remove(knjiga);
+
+	}
+
+	@Override
+	public List<Knjiga> VratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		if (autor == null && isbn <= 0 && naslov == null && izdavac == null)
+			throw new IllegalArgumentException("Morate uneti bar neki kriterijum pretrage");
+
+		List<Knjiga> rezultati = new ArrayList<Knjiga>();
+
+		for (Knjiga k : knjige)
+			if (k.getNaslov().toUpperCase().contains(naslov.toUpperCase()))
+				rezultati.add(k);
+
+		return rezultati;
+	}
+
+}
